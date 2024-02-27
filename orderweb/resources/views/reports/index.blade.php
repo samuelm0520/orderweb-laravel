535f06dccf93866329ed2bd241c4585255b5a690
samuel <samuelmm0520@gmail.com>
2024-02-27 12:34:33 -0500

    Report part 3

--- orderweb/resources/views/reports/index.blade.php
+++ orderweb/resources/views/reports/index.blade.php
@@ -30,43 +30,41 @@
                     </a>
             </div>
         </div>
-    </div>
     <!-- -------------------------------------------------------------------------------------- -->
-    <div class="row">
-        <div class="col-lg-12 mb-4">
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">
-                        Reporte actividades por tecnicos
-                    </h6>
-                </div>
-                <div class="card-body">
-                    <form action="{{ route('reports.activities_technician') }}" method="get">
-                        @csrf
-                        <div class="row form-group">
-                            <div class="col-lg-2">
-                                <label for="technician_id">Tecnico</label>
-                            </div>
-                            <div class="col-lg-5">
-                                <select name="technician_id" id="technician_id" class="form-control" required>
-                                    <option value="">Seleccione</option>
-                                    @foreach ( $technicians as $technician)
-                                        <option value="{{ $technician['document'] }}">
-                                            {{ $technician['name'] }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="col-lg-5">
-                                <button type="submit" class="btn btn-secondary btn-block col-lg-3 mb-4">
-                                    <i class="fa-solid fa-file-pdf"></i>
-                                </button>
-                            </div>
+        <div class="row">
+            <div class="col-lg-12 mb-4">
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">
+                            Reporte actividades por tecnico
+                        </h6>
+                        <div class="card-body">
+                            <form action="{{ route('reports.activities_technician') }}" method="get">
+                                @csrf
+                                <div class="row form-group">
+                                    <div class="col-lg-2">
+                                        <label for="technician_id">Tecnico</label>
+                                    </div>
+                                    <div class="col-lg-5">
+                                        <select name="technician_id" id="technician_id" class="form-control" required>
+                                            <option value="">Seleccione</option>
+                                            @foreach ( $technicians as $technician)
+                                                <option value="{{ $technician['document'] }}">
+                                                    {{ $technician['name'] }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col-lg-5">
+                                        <button type="submit" class="btn btn-secondary btn-block col-lg-3 mb-4">
+                                            <i class="fa-solid fa-file-pdf"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form> 
                         </div>
-                    </form> 
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-
 @endsection
+
